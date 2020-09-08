8bfc2b29579b1bf17dc12509a6c2b1f9adccf495
Adrian Garcia Rojas <adrian_gar016@hotmail.es>
2020-09-08 15:49:58 -0500

    Lecc 2, en esta lec. tendremos en cuenta las directivas que se implementan con -v son atributos de vue

--- js/vue1.js
+++ js/vue1.js
@@ -4,3 +4,10 @@ var app = new Vue({
         message:'hola mundo, ejemplos de Vue.js desde la documentacion oficial'
     }
 })
+
+var app2 = new  Vue({
+    el: '#app-2',
+    data:{
+        message: 'usted cargo esta pagina el ' + new Date().toLocaleString()
+    }
+})
