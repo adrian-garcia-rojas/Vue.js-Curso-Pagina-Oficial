3b0d88a43a84bd96c41319658a753ddf6c7bf282
Adrian Garcia Rojas <adrian_gar016@hotmail.es>
2020-09-09 10:29:10 -0500

    Lecc 6, Composicion con Componentes, este es un ejemplo ficticio de como se pueden separar dos unidades mas pequeñas y el componente hijo esta bien desacoplado del componente padre a travez de las interfaz de propiedades

--- js/vue1.js
+++ js/vue1.js
@@ -1,6 +1,15 @@
-var app6 = new Vue({
-    el: '#app-6',
+Vue.component('todo-item',{
+    props:['todo'],
+    template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+    el: '#app-7',
     data:{
-        message: 'hola Vue'
+        groceryList:[
+            {id: 0, text: 'Vegetales'},
+            {id: 1, text: 'Queso'},
+            {id: 2, text: 'Cualquier otra cosa que se supone que los humanos coman'}
+        ]
     }
 })
