3cd57dc95b4fe38c79a3fe8f834f903a06de5af3
Adrian Garcia Rojas <adrian_gar016@hotmail.es>
2020-09-09 09:50:26 -0500

    Lecc 5, Veamos como interactua la directiva v-model esto hace que el enlace bidireccional entre los controles de los formularios y el estado de la aplicacion se muy sencilla HACIENDO TWO-WAY BINDING algo asi

--- js/vue1.js
+++ js/vue1.js
@@ -1,11 +1,6 @@
-var app5 = new Vue({
-    el: '#app-5',
+var app6 = new Vue({
+    el: '#app-6',
     data:{
-        message: 'hola Vue.js'
-    },
-    methods:{
-        reverseMessage: function(){
-            this.message = this.message.split('').reverse().join('')
-        }
+        message: 'hola Vue'
     }
 })
