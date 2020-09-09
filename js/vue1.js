cd3e71dc25e6e952bef2f6743ce09afcadd4cc69
Adrian Garcia Rojas <adrian_gar016@hotmail.es>
2020-09-09 15:23:36 -0500

    Lecc 9, Sintais de template, conienen todos los temas de la documentacion por ej. interpolaciones, directivas, modo abreviado. Adicional complemente los ejercicios

--- js/vue1.js
+++ js/vue1.js
@@ -1,15 +1,91 @@
-/* Hooks del ciclo de vida de la instancia 
-created,
-mounted,
-updated,
-destroyed
-*/
-
-var vm = new Vue({
+/* Interpolaciones */
+var texto = new Vue({
+    el: '#texto',
     data: {
-        a:1
-    },
-    created: function(){
-        console.log('a es: ' + this.a)
+        mensaje: 'interpolacion de texto'
     }
 })
+
+/* html puro */
+var htmlPuro = new Vue({
+    el:'#htmlPuro',
+    data:{
+        rawHtml:' -> con html puro y mustachess {{}} / ver direciva v-html'
+    }
+})
+
+/* Atributos */
+var atributos = new Vue({
+    el:'#atributos',
+    data:{
+        dynamicId: 12,
+        isButtonDisabled: true
+    }
+})
+
+/* Usando expresiones JavaScript */
+var expresionesJS = new Vue({
+    el: '#expresionesJS',
+    data: {
+        number: 1,
+        ok: false,
+        message: 'hola mundo'
+    }
+})
+
+
+/*DIRECTIVAS */
+var directivas = new Vue({
+    el: '#directivas',
+    data: {
+        seen: true
+    }
+})
+
+
+/*Argumentos */
+var argumentos = new Vue({
+    el: '#argumentos',
+    data: {
+        url: 'https://www.google.com.mx',
+        mensaje: 'saludo'
+    },
+    methods: {
+        doSomething: function(){
+            alert(this.mensaje)
+        }
+    }
+})
+
+
+/*Modificadores */
+var modificadores = new Vue({
+    el: '#modificadores',
+    data: {
+        mensaje: 'mensaje desde modificadores'
+    },
+    methods: {
+        onSubmit: function(){
+            console.log(this.mensaje)
+        }
+    }
+})
+
+
+/** Revisar los modos abreviados de 
+ * v-bind & v-on
+ *
+ * full v-bind 
+ *      <a v-bind:href="url">...</a>
+ * abreviado
+ *      <a :href="url">...</a>
+ * 
+ * 
+ * full v-on
+ * 
+ *      <a v-on:click="doSomething">...</a>
+ * 
+ * abreviado
+ *      <a @click="doSomething">...</a>
+ * 
+ */
