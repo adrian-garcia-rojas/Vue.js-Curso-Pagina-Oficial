bcddcafdf186a0a3170a36abbfeb89f3d9f6bb66
Adrian Garcia Rojas <adrian_gar016@hotmail.es>
2020-09-09 17:19:46 -0500

    Lecc 10, Propiedades computadas, ejemplo basico donde se enlazan datos a las propiedades computadas en el template al igual que en la propiedad normal. ver la relacion de dependecia que se crea de manera declarativa la funcion getter en este caso reversedMessage no tiene efectos secundarios, lo que facilita la prueba y comprencion y actualizara todos los enlaces que dependan de vm.reversedMessage

--- js/vue1.js
+++ js/vue1.js
@@ -1,91 +1,11 @@
-/* Interpolaciones */
-var texto = new Vue({
-    el: '#texto',
+var vm = new Vue({
+    el: '#example',
     data: {
-        mensaje: 'interpolacion de texto'
-    }
-})
-
-/* html puro */
-var htmlPuro = new Vue({
-    el:'#htmlPuro',
-    data:{
-        rawHtml:' -> con html puro y mustachess {{}} / ver direciva v-html'
-    }
-})
-
-/* Atributos */
-var atributos = new Vue({
-    el:'#atributos',
-    data:{
-        dynamicId: 12,
-        isButtonDisabled: true
-    }
-})
-
-/* Usando expresiones JavaScript */
-var expresionesJS = new Vue({
-    el: '#expresionesJS',
-    data: {
-        number: 1,
-        ok: false,
-        message: 'hola mundo'
-    }
-})
-
-
-/*DIRECTIVAS */
-var directivas = new Vue({
-    el: '#directivas',
-    data: {
-        seen: true
-    }
-})
-
-
-/*Argumentos */
-var argumentos = new Vue({
-    el: '#argumentos',
-    data: {
-        url: 'https://www.google.com.mx',
-        mensaje: 'saludo'
+        message: 'hola'
     },
-    methods: {
-        doSomething: function(){
-            alert(this.mensaje)
+    computed:{
+        reversedMessage: function(){
+            return this.message.split('').reverse().join('')
         }
     }
 })
-
-
-/*Modificadores */
-var modificadores = new Vue({
-    el: '#modificadores',
-    data: {
-        mensaje: 'mensaje desde modificadores'
-    },
-    methods: {
-        onSubmit: function(){
-            console.log(this.mensaje)
-        }
-    }
-})
-
-
-/** Revisar los modos abreviados de 
- * v-bind & v-on
- *
- * full v-bind 
- *      <a v-bind:href="url">...</a>
- * abreviado
- *      <a :href="url">...</a>
- * 
- * 
- * full v-on
- * 
- *      <a v-on:click="doSomething">...</a>
- * 
- * abreviado
- *      <a @click="doSomething">...</a>
- * 
- */
