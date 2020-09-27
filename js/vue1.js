272a27d32c4055b3dd83e7a642bfea09a30fd98c
adrian garcia rojas <adrian_gar016@hotmail.es>
2020-09-27 11:35:56 -0500

    Lecc 14, Enlace de estilos de linea, (style) hay que tener en cuenta que para aplicar estos estilos hay que hacer uso tanto -> camelCase como kebab-case (use comillas con kebab-case) para los nombres de propiedades CSS

--- js/vue1.js
+++ js/vue1.js
@@ -1,83 +1,35 @@
-//Sintaxis de objeto
-var sintaxis = new Vue({
-    el: '#sintaxis',
+//###  Enlace de estilos en linea  ###
+
+// # sintaxis de objeto
+var sintaxisDeObjecto = new Vue({
+    el: '#sintaxisDeObjecto',
     data:{
-        isActive: true,
-        hasError: false
+        activeColor: 'red',
+        fontSize: 12
     }
 })
 
-var sintaxis2 = new Vue({
-    el: '#sintaxis2',
+var sintaxisDeObjetoMasObjectoDeEstilos = new Vue({
+    el: '#sintaxisDeObjetoMasObjectoDeEstilos',
     data:{
-        classObject:{
-            active: false,
-            'text-danger': false
+        styleObject:{
+            color: 'purple',
+            fontSize: '20px'
         }
     }
 })
 
-var sintaxis3Pcomputada = new Vue({
-    el: '#sintaxis3Pcomputada',
+// # Sintaxis de arreglo
+var sintaxisDeArreglo = new Vue({
+    el: '#sintaxisDeArreglo',
     data:{
-        isActive: true,
-        error: null
-    },
-    computed:{
-        classObject: function(){
-            return {
-                active: this.isActive && !this.error,
-                'text-danger': this.error && this.error.type === 'fatal'
-            }
+        baseStyle: {            
+            fontSize: '16px',
+            textTransform: 'uppercase'            
+        },
+        overridingStyles: {
+            color: 'blue',
+            fontStyle: 'italic'
         }
-    }
-})
-
-//Sintaxis de Array
-
-var sintaxisDeArray = new Vue({
-    el: '#sintaxisDeArray',
-    data:{
-        activeClass: 'active',
-        errorClass: 'text-danger'
-    }
-})
-
-var sintaxisDeArrayTernario = new Vue({
-    el: '#sintaxisDeArrayTernario',
-    data:{
-        isActive: true,
-        activeClass: 'active',
-        errorClass: 'text-danger'
-    }
-})
-
-var sintaxisDeArrayObject = new Vue({
-    el: '#sintaxisDeArrayObject',
-    data:{
-        isActive: true,
-        activeClass: 'active',
-        errorClass: 'text-dark' //error class siempre se activara en este ejemplo no quiere decir no podemos manipular su aparicion
-    }
-})
-
-// Con Componentes
-
-Vue.component('my-component', {
-    template: '<p class="foo bar">Hi, esto es un componente</p>'
-})
-
-var conComponent = new Vue({
-    el: '#conComponent'
-})
-
-Vue.component('my-component2',{
-    template: '<p class="foo bar">Hi, esto es un componente2</p>'
-})
-
-var conComponent2 = new Vue({
-    el: '#conComponent2',
-    data:{
-        isActive: false
     }
 })
