847c1cd36a5cbb39765fa5563292e7eb9651ff4f
adrian garcia rojas <adrian_gar016@hotmail.es>
2020-09-27 09:47:42 -0500

    Lecc 13, Enlace de Clases HTML, contiene los temas de  sintaxis de objeto, sintaxis de array, con Components, ver como interactuan las clases CSS

--- js/vue1.js
+++ js/vue1.js
@@ -1,119 +1,83 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        message: 'hola'
-    },
-    methods:{
-        reversedMessage: function(){
-            return this.message.split('').reverse().join('')
-        }
-    },
-    computed:{
-        now: function(){
-            return Date.now()
+//Sintaxis de objeto
+var sintaxis = new Vue({
+    el: '#sintaxis',
+    data:{
+        isActive: true,
+        hasError: false
+    }
+})
+
+var sintaxis2 = new Vue({
+    el: '#sintaxis2',
+    data:{
+        classObject:{
+            active: false,
+            'text-danger': false
         }
     }
 })
 
-
-/** CODIGO PARA DEMO
- * CONSTRUIDO DE FORMA IMPERATIVA
- */
-
- var vm = new Vue({
-     el: '#demo',
-     data:{
-         firstName:'Foo',
-         lastName:'Bar',
-         fullName:'Foo Bar'
-     },
-     watch:{
-         firstName: function(val){
-             this.fullName = val + ' ' + this.lastName
-         },
-         lastName: function(val){
-             this.fullName = this.firstName + ' ' + val
-         }
-     }
- })
-
-/** EL CODIGO ANTERIOR ES IMPERATIVO Y REPETITIVO. COMPARA CON UNA VERSION DE PROPIEDAD COMPUTADA
- * 
- * MUCHO MEJOR NO?
- */
-var vm = new Vue({
-    el: '#demo',
+var sintaxis3Pcomputada = new Vue({
+    el: '#sintaxis3Pcomputada',
     data:{
-        firstName:'Foo',
-        lastName:'Bar'
+        isActive: true,
+        error: null
     },
     computed:{
-        fullName: function(){
-            return this.firstName + ' ' + this.lastName
-        }
-    }
-})
-
-
-/**Setter Computado */
-var vmsc = new Vue({
-    el: '#setterComputado',
-    data:{
-        firstName: 'Foo',
-        lastName: 'Bar'
-    },
-    computed:{
-        fullName2:{
-            get: function(){
-                return this.firstName + ' ' + this.lastName
-            },
-            set: function(newValue){
-                var names = newValue.split(' ')
-                this.firstName = names[0]
-                this.lastName = names[names.length - 1]
+        classObject: function(){
+            return {
+                active: this.isActive && !this.error,
+                'text-danger': this.error && this.error.type === 'fatal'
             }
         }
     }
 })
 
-/**Watchers */
-var watchExampleVM = new Vue({
-    el: '#watch-example',
+//Sintaxis de Array
+
+var sintaxisDeArray = new Vue({
+    el: '#sintaxisDeArray',
     data:{
-        question: '',
-        answer: 'No puedo darte una respuesta hasta que hagas una pregunta!'
-    },
-    watch:{
-        question: function(newQuestion, oldQuestion){
-            this.answer = 'Esperando que deje de escribri...'
-            this.debouncedGetAnswer()
-        }
-    },
-    created: function(){
-        // _.debounce es una función proporcionada por lodash para limitar cuan
-        // a menudo se puede ejecutar una operación particularmente costosa.
-        // En este caso, queremos limitar la frecuencia con la que accedemos a
-        // yesno.wtf/api, esperando hasta que el usuario haya terminado
-        // de escribir antes de realizar la solicitud ajax.
-        // Para aprender más sobre la función _.debounce (y su primo
-        // _.throttle), visite: https://lodash.com/docs#debounce
-        this.debouncedGetAnswer = _.debounce(this.getAnswer, 500)
-    },
-    methods:{
-        getAnswer: function(){
-            if(this.question.indexOf('?') === -1){
-                this.answer = 'Las preguntas suelen contener un signo de interrogacion'
-                return
-            }
-            this.answer = 'Pensando...'
-            var vm = this
-            axios.get('https://yesno.wtf/api')
-                .then(function(response){
-                    vm.answer = _.capitalize(response.data.answer)
-                })
-                .catch(function(error){
-                    vm.answer = '¡Error! No se pudo alcanzar la API. ' + error
-                })
-        }
+        activeClass: 'active',
+        errorClass: 'text-danger'
+    }
+})
+
+var sintaxisDeArrayTernario = new Vue({
+    el: '#sintaxisDeArrayTernario',
+    data:{
+        isActive: true,
+        activeClass: 'active',
+        errorClass: 'text-danger'
+    }
+})
+
+var sintaxisDeArrayObject = new Vue({
+    el: '#sintaxisDeArrayObject',
+    data:{
+        isActive: true,
+        activeClass: 'active',
+        errorClass: 'text-dark' //error class siempre se activara en este ejemplo no quiere decir no podemos manipular su aparicion
+    }
+})
+
+// Con Componentes
+
+Vue.component('my-component', {
+    template: '<p class="foo bar">Hi, esto es un componente</p>'
+})
+
+var conComponent = new Vue({
+    el: '#conComponent'
+})
+
+Vue.component('my-component2',{
+    template: '<p class="foo bar">Hi, esto es un componente2</p>'
+})
+
+var conComponent2 = new Vue({
+    el: '#conComponent2',
+    data:{
+        isActive: false
     }
 })
